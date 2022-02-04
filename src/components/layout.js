bb202753e77e69b54a55b6950bc7b45cbc7f4dab
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-04 10:05:29 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,12 @@
 import React from "react"
 
-import { GlobalStyles, Head, MessageTop, Header, Footer, Container } from "@components"
+import { GlobalStyles, Head, Header, Footer, Container } from "@components"
 
 export default function Layout({ title, children }) {
   return (
     <div>
       <GlobalStyles />
       <Head title={title} />
-      <MessageTop />
       <Header />
       <Container
         as="main"
